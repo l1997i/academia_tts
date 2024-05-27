b05a13a24dd45490c8c46b9798b89efd07cd8f9e
Luis Li <li.li4@durham.ac.uk>
2024-05-28 00:48:00 +0100

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='academia_tts',
-    version='0.2.3',
+    version='0.2.4',
     packages=find_packages(),
     install_requires=[
         'torch',
